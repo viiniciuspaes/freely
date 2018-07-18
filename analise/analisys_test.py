4a45e7f44e21520ddd1cb7d1924c7f6fff76b683
Francisco Queiroga <franciscofqueiroga@gmail.com>
2018-07-17 21:16:14 -0300

    [TEST] [NB] Correções de uso e novos testes
    Primeiras correções e exemplos mais fortes do modelo sendo treinado para predição dos sentimentos

---
+++ analise/analisys_test.py
@@ -0,0 +1,25 @@
+from fit_test import vectorizer, modelo
+
+# instâncias de teste
+
+testes = ['Esse governo está no início, vamos ver o que vai dar',
+          'Estou muito feliz com o governo de Minas esse ano',
+          'Penso em destruir esse país por inteiro, essa merda',
+          'Eu quero que Lula se foda, esse merda',
+          'Minha família ama o PT, eu não',
+          'Vão todos se fuder!',
+          'Eu amo o Moro, quero casar com ele',
+          'Gosto do Bolsonaro, ele é um gênio',
+          'O estado de Minas Gerais decretou calamidade financeira!!!',
+          'A segurança desse país está deixando a desejar',
+          'O governador de Minas é do PT',
+          'Eu adoro este governo, ele está indo muito bem']
+
+
+# inicio dos testes a partir do modelo
+
+freq_testes = vectorizer.transform(testes)
+
+# classificação com o modelo treinado
+
+print(modelo.predict(freq_testes))
